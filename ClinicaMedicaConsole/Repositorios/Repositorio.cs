ca9905dfcdd93815e7bdc10f269c66cf9bed991f
Alainderson <113845091+Alainderson@users.noreply.github.com>
2026-02-14 16:38:05 -0300

    Add comment for testing in Repositorio class

--- ClinicaMedicaConsole/Repositorios/Repositorio.cs
+++ ClinicaMedicaConsole/Repositorios/Repositorio.cs
@@ -8,4 +8,5 @@ public class Repositorio<T> where T : class
     private readonly SqlConnection _connection;
     public Repositorio(SqlConnection connection) => _connection = connection;
     public void Create(T model) => _connection.Insert<T>(model);
+    /*Testando */
 }
